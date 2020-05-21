6366e7ecbccdce8c9cead50f594e0ddd11296eab
Remion Bacova <remion.bacova@live.com>
2020-05-21 10:39:37 +0200

    PrimaryPublish

---
+++ TaskManagement/1- CreateReporting Structure.sql
@@ -0,0 +1,47 @@
+﻿USE [TaskManagement]
+GO
+/****** Object:  Table [dbo].[tbl_MachineReporting]    Script Date: 21/05/2020 10:24:47 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[tbl_MachineReporting](
+	[UID] [int] IDENTITY(1,1) NOT NULL,
+	[MachineUID] [int] NOT NULL,
+	[ProcessName] [nvarchar](100) NULL,
+	[ApplicationName] [nvarchar](100) NULL,
+	[TotalSecond] [int] NULL,
+	[CreationDate] [datetime] NULL,
+ CONSTRAINT [PK_tbl_MachineReporting] PRIMARY KEY CLUSTERED 
+(
+	[UID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[tbl_Machines]    Script Date: 21/05/2020 10:24:47 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[tbl_Machines](
+	[UID] [int] IDENTITY(1,1) NOT NULL,
+	[MachineName] [nvarchar](100) NULL,
+	[OsVersion] [nvarchar](100) NULL,
+	[UserDomainName] [nvarchar](100) NULL,
+	[UserName] [nvarchar](100) NULL,
+	[Version] [nvarchar](100) NULL,
+	[MachineHash] [nvarchar](100) NULL,
+	[Active] [bit] NULL,
+	[CreationDate] [datetime] NULL,
+	[UserUID] [int] NULL,
+ CONSTRAINT [PK_tbl_Machines] PRIMARY KEY CLUSTERED 
+(
+	[UID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[tbl_MachineReporting]  WITH CHECK ADD  CONSTRAINT [FK_tbl_MachineReporting_tbl_Machines] FOREIGN KEY([MachineUID])
+REFERENCES [dbo].[tbl_Machines] ([UID])
+GO
+ALTER TABLE [dbo].[tbl_MachineReporting] CHECK CONSTRAINT [FK_tbl_MachineReporting_tbl_Machines]
+GO
---
+++ TaskManagement/2-Create Reporting Stored Procedures.sql
@@ -0,0 +1,90 @@
+﻿GO
+/****** Object:  StoredProcedure [dbo].[spi_tbl_MachineReporting]    Script Date: 21/05/2020 10:26:00 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+CREATE PROCEDURE [dbo].[spi_tbl_MachineReporting] ( 
+
+@ProcessName nvarchar(50),
+@ApplicationName nvarchar(14),
+@TotalSecond nvarchar(10),       
+@MachineHash nvarchar(100)
+
+							  
+										  )
+	                                      
+AS 
+    
+DECLARE @MachineUID int
+SET @MachineUID = (select P.UID from tbl_Machines P where P.MachineHash= @MachineHash )
+
+
+	
+        BEGIN  
+				INSERT INTO [dbo].[tbl_MachineReporting]
+					   ([MachineUID]
+					   ,[ProcessName]
+					   ,[ApplicationName]
+					   ,[TotalSecond]
+					   ,[CreationDate])
+				VALUES
+					   (@MachineUID
+					   ,@ProcessName
+					   ,@ApplicationName
+					   ,@TotalSecond
+					   ,getdate())
+
+        END  
+  
+GO
+/****** Object:  StoredProcedure [dbo].[spi_tbl_Machines]    Script Date: 21/05/2020 10:26:00 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[spi_tbl_Machines] ( 
+                                          @MachineName   VARCHAR(100),  
+                                          @OsVersion    VARCHAR(100),  
+                                          @UserDomainName       VArchar(100),  
+                                          @UserName      VARCHAR(100),  
+										  @Version      VARCHAR(100),
+										  @MachineHash varchar(100)
+
+										  )
+	                                      
+AS  
+    
+	if((Select COunt(*) from tbl_Machines where @MachineHash = MachineHash ) <1)
+
+
+	
+        BEGIN  
+
+		INSERT INTO [dbo].[tbl_Machines]
+           ([MachineName]
+           ,[OsVersion]
+           ,[UserDomainName]
+           ,[UserName]
+           ,[Version]
+           ,[MachineHash]
+           ,[Active]
+           ,[CreationDate]
+           ,[UserUID])
+     VALUES
+           (@MachineName
+           ,@OsVersion
+           ,@UserDomainName
+           ,@UserName 
+           , @Version
+           ,@MachineHash
+           ,1
+           ,getdate()
+           ,1)
+
+        END  
+  
+GO
