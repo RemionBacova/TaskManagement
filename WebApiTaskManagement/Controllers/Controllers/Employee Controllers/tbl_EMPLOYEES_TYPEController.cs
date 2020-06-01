0d81dec367b69359f31dfff71f3f858bee7beb55
xhovana22 <xgjinaj16@epoka.edu.al>
2020-06-01 22:39:11 +0200

    added the rest of the controllers.

---
+++ WebApiTaskManagement/Controllers/Controllers/Employee Controllers/tbl_EMPLOYEES_TYPEController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiTaskManagement.Repository.Base.EntitiesRepository;
+
+namespace WebApiTaskManagement.Controllers.Users_Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class tbl_EMPLOYEES_TYPEController : Abstract.Base.tbl_TABLE_TYPE_ControllerAbstract
+    {
+        public tbl_EMPLOYEES_TYPEController(sp_tbl_TABLE_TYPE_Repository repository) : base(repository, "EMPLOYEES")
+        {
+
+        }
+    }
+}
