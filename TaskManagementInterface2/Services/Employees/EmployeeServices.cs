470c2653ad8be016580d4606ab51d7294a1afbf3
Jonel Laho <51534362+jonellaho@users.noreply.github.com>
2020-07-16 11:40:56 +0200

    empoyes

---
+++ TaskManagementInterface2/Services/Employees/EmployeeServices.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TaskManagementInterface.Data.EMPLOYEES;
+
+namespace TaskManagementInterface.Services.Employees
+{
+    public class EmployeeServices
+    {
+        public HttpClient http = new HttpClient();
+        public async Task<List<tbl_Employee>> SelectAllActiveRec()
+        {
+            return await http.GetJsonAsync<List<tbl_Employee>>("http://192.168.1.109/api/tbl_EMPLOYEES");
+        }
+    }
+}
