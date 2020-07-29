6edab1e63f70c23d827631cf790793e01b41f1b4
Jonel Laho <51534362+jonellaho@users.noreply.github.com>
2020-07-29 15:00:08 +0200

    fdgdfgdf

--- TaskManagementInterface2/Services/TypeInfo/TypeInfoServices.cs
+++ TaskManagementInterface2/Services/TypeInfo/TypeInfoServices.cs
@@ -39,7 +39,7 @@ namespace TaskManagementInterface.Services.TypeInfo
         public async Task<Error> Add(tbl_TABLE_TYPE_INFO TypeModel, string tableName, string parameters)
         {
             //Property duhet te plotesohet 
-            string url = "http://192.168.1.109/api/" + tableName +  "/" + TypeModel.nomination+"/1"   + parameters;
+            string url = "http://192.168.1.109/api/" + tableName +  "/" + TypeModel.nomination+"/" +TypeModel.property   + parameters;
             try
             {
                 List<Error> list = await http.PostJsonAsync<List<Error>>(url, "");
@@ -55,7 +55,7 @@ namespace TaskManagementInterface.Services.TypeInfo
 
         public async Task<Error> Update(tbl_TABLE_TYPE_INFO kategoriModel, string tableName, string parameters)
         {
-            string url = "http://192.168.1.109/api/" + tableName + "/" + kategoriModel.uid+"/"+kategoriModel.type_uid + "/" + kategoriModel.nomination+ "/2"+ parameters;
+            string url = "http://192.168.1.109/api/" + tableName + "/" + kategoriModel.uid+"/"+kategoriModel.type_uid + "/" + kategoriModel.nomination+ "/"+ kategoriModel.property + parameters;
             try
             {
                 List<Error> list = await http.PutJsonAsync<List<Error>>(url, "");
