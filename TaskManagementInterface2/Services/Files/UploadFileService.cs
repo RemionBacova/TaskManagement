12f0f7b1d497bb8a70b967287cb7aed2d283ebc2
Jonel Laho <51534362+jonellaho@users.noreply.github.com>
2020-08-05 18:02:07 +0200

    new

---
+++ TaskManagementInterface2/Services/Files/UploadFileService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TaskManagementInterface.Data;
+
+namespace TaskManagementInterface.Services.Files
+{
+    public class UploadFileService
+    {
+        public HttpClient http = new HttpClient();
+
+        public async Task<Error> AddInfo(string Entity, string ElementID, string TypeInfoID)
+        {
+
+            string url = "http://192.168.1.109:81/api/File/upload?entity="+Entity+ "&elementID=" + ElementID + "&typeinfoID="+TypeInfoID;
+
+            try
+            {
+                List<Error> list = await http.PostJsonAsync<List<Error>>(url, "");
+                return list.FirstOrDefault();
+            }
+            catch (Exception ex)
+            {
+                Error error = new Error();
+                error.ERRORDESCRIPTION = ex.ToString();
+                return error;
+            }
+        }
+    }
+}
