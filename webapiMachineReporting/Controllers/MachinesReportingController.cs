8c74d51706cb625e40a1d089af0283007ae9623d
arbso <pentaxonn@gmail.com>
2020-08-05 17:39:24 +0200

    webapiMachineReporting

---
+++ webapiMachineReporting/Controllers/MachinesReportingController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webapiMachineReporting.Models;
+using webapiMachineReporting.Repository;
+
+namespace webapiMachineReporting.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MachinesReportingController : ControllerBase
+    {
+        private MachineReportingRepository _repository;
+
+        public MachinesReportingController(MachineReportingRepository repository)
+        {
+            this._repository = repository; /*?? throw new ArgumentNullException(nameof(repository));*/
+
+        }
+
+        //Post api/machine
+        [HttpPost]
+        public async Task spi_tbl_MachineReporting([FromBody] MachineReporting machinerep)
+        {
+            await _repository.spi_tbl_MachineReporting(machinerep);
+
+
+        }
+
+        [HttpGet("GetAll_MachineRecordingByUID")]
+        public async Task<IEnumerable<MachineReporting2>> SelectMachineReportingByUID(string uid, string dateBegin, string dateEnd)
+        {
+            return await _repository.SelectMachineReportingByUID(uid, dateBegin, dateEnd);
+        }
+    }
+}
---
+++ webapiMachineReporting/Repository/MachineReportingRepository.cs
@@ -0,0 +1,59 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using webapiMachineReporting.Models;
+
+namespace webapiMachineReporting.Repository
+{
+    public class MachineReportingRepository
+    {
+        private readonly string _constring;
+        public MachineReportingRepository(IConfiguration configuration)
+        {
+            _constring = configuration.GetConnectionString("defaultConnection");
+        }
+
+
+        public async Task<IEnumerable<MachineReporting>> spi_tbl_MachineReporting(MachineReporting machinerep)
+        {
+
+            using (IDbConnection sql = new SqlConnection(_constring))
+            {
+
+                string readSp = "spi_tbl_MachineReporting";
+                var queryParameters = new DynamicParameters();
+
+
+                queryParameters.Add("@ProcessName", machinerep.ProcessName);
+                queryParameters.Add("@ApplicationName", machinerep.ApplicationName);
+                queryParameters.Add("@TotalSecond", machinerep.TotalSeconds);
+                queryParameters.Add("@MachineHash", machinerep.MachineHash);
+                queryParameters.Add("@SessionID", machinerep.GUID);
+
+                return await sql.QueryAsync<MachineReporting>(readSp, queryParameters, commandType: CommandType.StoredProcedure);
+            }
+
+        }
+
+        public async Task<IEnumerable<MachineReporting2>> SelectMachineReportingByUID(string uid, string dateBegin, string dateEnd)
+        {
+            using (IDbConnection db = new SqlConnection(_constring))
+            {
+                string readSp = "SelectAllActiveMachineReporting";
+                var queryParameters = new DynamicParameters();
+                queryParameters.Add("@machineUID", uid);
+                queryParameters.Add("@dateBegin", dateBegin);
+                queryParameters.Add("@dateEnd", dateEnd);
+
+                return await db.QueryAsync<MachineReporting2>(readSp, queryParameters, commandType: CommandType.StoredProcedure);
+            }
+
+
+        }
+    }
+}
