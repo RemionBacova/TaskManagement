37c98602ce9ccc8e4e43336f82cc9bf219fed52c
Jonel Laho <51534362+jonellaho@users.noreply.github.com>
2020-08-06 17:43:46 +0200

    trhgf

--- TaskManagementInterface2/Services/Files/UploadFileService.cs
+++ TaskManagementInterface2/Services/Files/UploadFileService.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using TaskManagementInterface.Data;
 
@@ -19,24 +20,21 @@ namespace TaskManagementInterface.Services.Files
 
         public async Task AddInfoAsync([FromForm(Name = "files")] List<Syncfusion.Blazor.Inputs.Internal.UploadFiles> files, string Entity, string ElementID, string TypeInfoID)
         {
+            string urla = "http://192.168.1.109:81/api/File/upload?entity=" + Entity + "&elementID=" + ElementID + "&typeinfoID=" + TypeInfoID;
+            try
+            {
+                var file = files[0];
+                var ms = file.Stream;
 
-            string url = "http://192.168.1.109:81/api/File/upload?entity="+Entity+ "&elementID=" + ElementID + "&typeinfoID="+TypeInfoID;
-
-            
-           await http.PostJsonAsync(url,"");
-
-            //try
-            //{
-            //    List<Error> list = await http.PostJsonAsync<List<Error>>(url, "");
-            //    return list.FirstOrDefault();
-            //}
-            //catch (Exception ex)
-            //{
-            //    Error error = new Error();
-            //    error.ERRORDESCRIPTION = ex.ToString();
-            //    return error;
-            //}
-            
+                var content = new MultipartFormDataContent {
+                { new ByteArrayContent(ms.GetBuffer()), "\"upload\"", file.FileInfo.Name }
+            };
+                await http.PostAsync(urla, content);
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
         }
     }
 }
