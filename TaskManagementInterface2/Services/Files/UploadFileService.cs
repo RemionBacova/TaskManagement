62fd3468353cec9f82fa701eea74014749db72d1
Jonel Laho <51534362+jonellaho@users.noreply.github.com>
2020-08-06 16:39:57 +0200

    mjg

--- TaskManagementInterface2/Services/Files/UploadFileService.cs
+++ TaskManagementInterface2/Services/Files/UploadFileService.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Syncfusion.Blazor;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -12,22 +17,26 @@ namespace TaskManagementInterface.Services.Files
     {
         public HttpClient http = new HttpClient();
 
-        public async Task<Error> AddInfo(string Entity, string ElementID, string TypeInfoID)
+        public async Task AddInfoAsync([FromForm(Name = "files")] List<Syncfusion.Blazor.Inputs.Internal.UploadFiles> files, string Entity, string ElementID, string TypeInfoID)
         {
 
             string url = "http://192.168.1.109:81/api/File/upload?entity="+Entity+ "&elementID=" + ElementID + "&typeinfoID="+TypeInfoID;
 
-            try
-            {
-                List<Error> list = await http.PostJsonAsync<List<Error>>(url, "");
-                return list.FirstOrDefault();
-            }
-            catch (Exception ex)
-            {
-                Error error = new Error();
-                error.ERRORDESCRIPTION = ex.ToString();
-                return error;
-            }
+            
+           await http.PostJsonAsync(url,"");
+
+            //try
+            //{
+            //    List<Error> list = await http.PostJsonAsync<List<Error>>(url, "");
+            //    return list.FirstOrDefault();
+            //}
+            //catch (Exception ex)
+            //{
+            //    Error error = new Error();
+            //    error.ERRORDESCRIPTION = ex.ToString();
+            //    return error;
+            //}
+            
         }
     }
 }
